6ee9e75b421b87afb7d01f3f98e47e34cabe0352
DeveloperAchu <achuunnikrishnan1@gmail.com>
2020-03-24 00:51:50 +0000

    change file type to binary to use pickle

--- InitialStates.py
+++ InitialStates.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import pickle
 
 from CubeRotations import perform_cube_operations
 from DefineStates import *
@@ -32,39 +33,42 @@ else:
 n = 3
 sides_list = []
 
-file_name = str(n) + "x" + str(n) + ".txt"
-with open(file_name, "w") as file:
-    states = []  #
-    for state in range(total_states):
-        details = []  #
-        moves = []  #
+states = []  #
+list_of_sides = []
+for state in range(total_states):
+    details = []  #
+    moves = []  #
 
-        sides = []
-        actual_moves = 0  #
+    sides = []
+    actual_moves = 0  #
 
-        sides.append(np.reshape([0 for _ in range(n ** 2)], (n, n)))
-        sides.append(np.reshape([1 for _ in range(n ** 2)], (n, n)))
-        sides.append(np.reshape([2 for _ in range(n ** 2)], (n, n)))
-        sides.append(np.reshape([3 for _ in range(n ** 2)], (n, n)))
-        sides.append(np.reshape([4 for _ in range(n ** 2)], (n, n)))
-        sides.append(np.reshape([5 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([0 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([1 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([2 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([3 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([4 for _ in range(n ** 2)], (n, n)))
+    sides.append(np.reshape([5 for _ in range(n ** 2)], (n, n)))
 
-        scramble_size = random.randint(1, max_scramble_size)
-        for scramble in range(scramble_size):
-            cube_parameters = get_a_state_change()
-            cube_slice = get_cube_slice(cube_parameters[0], cube_parameters[1])
-            axis = get_cube_axis(cube_parameters[2], cube_parameters[3])
-            rotation = get_cube_rotation(cube_parameters[4], cube_parameters[5])
-            if cube_slice != 0 and axis is not None and rotation != 0:
-                actual_moves += 1  #
-                moves.append((cube_slice, axis, rotation))  #
-                perform_cube_operations(n, sides, cube_slice, axis, rotation)
-        details.append("state: " + str(state))  #
-        details.append("max scrambles: " + str(scramble_size))  #
-        details.append("actual moves: " + str(actual_moves))  #
-        details.append("moves: " + str(moves))  #
-        states.append(details)  #
-        file.write("\nc:" + str(convert_sides(sides)))
+    scramble_size = random.randint(1, max_scramble_size)
+    for scramble in range(scramble_size):
+        cube_parameters = get_a_state_change()
+        cube_slice = get_cube_slice(cube_parameters[0], cube_parameters[1])
+        axis = get_cube_axis(cube_parameters[2], cube_parameters[3])
+        rotation = get_cube_rotation(cube_parameters[4], cube_parameters[5])
+        if cube_slice != 0 and axis is not None and rotation != 0:
+            actual_moves += 1  #
+            moves.append((cube_slice, axis, rotation))  #
+            perform_cube_operations(n, sides, cube_slice, axis, rotation)
+    details.append("state: " + str(state))  #
+    details.append("max scrambles: " + str(scramble_size))  #
+    details.append("actual moves: " + str(actual_moves))  #
+    details.append("moves: " + str(moves))  #
+    states.append(details)  #
+    list_of_sides.append(convert_sides(sides))
+
+file_name = str(n) + "x" + str(n)
+with open(file_name, "wb") as file:
+    pickle.dump(list_of_sides, file)
 file.close()
 
 #
